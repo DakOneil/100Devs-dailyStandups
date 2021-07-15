da3a6f482981f4532c1d0db6cabe4943e1a7e9fb
Dakota O'Neil <oneilkody@gmail.com>
2021-07-15 08:44:35 -0700

    add todays standup sol

--- dailyStandupsWk2.js
+++ dailyStandupsWk2.js
@@ -76,3 +76,27 @@ function domainName(str) {
   str = str.replace("www.", '');
   return str.split('.')[0];
 }
+
+
+
+
+//July 15, 2021
+
+// Given a string, return a new string that has transformed based on the input:
+
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// Note: You will have to handle multiple spaces, and leading/trailing spaces.
+
+// For example:
+
+// "Example Input" ==> "iNPUT eXAMPLE"
+// You may assume the input only contain English alphabet and spaces.
+
+function stringTransformer(str) {
+  // Your code here
+  return str.split(' ').reverse().join(' ').split('').map((el)=> {
+      if (el == el.toUpperCase()) return el.toLowerCase()
+      else if (el == el.toLowerCase()) return el.toUpperCase()
+    }).join('')
+}
