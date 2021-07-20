c5444acc33b7d3cc461d4ec603100af1e45cddaf
Dakota O'Neil <oneilkody@gmail.com>
2021-07-19 19:35:18 -0500

    add the standup sol from the 20th

--- dailyStandupsWk3.js
+++ dailyStandupsWk3.js
@@ -26,3 +26,31 @@ function high(x){
     } let ind = points.indexOf(Math.max(...points))
     return words[ind]
   }
+
+
+
+
+
+// July 20, 2021
+
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+function duplicateEncode(word){
+    // ...
+  let codeWord = ''
+  word = word.toLowerCase()
+  for (let i = 0 ; i < word.length ; i++) {
+    if (word.indexOf(word[i]) === word.lastIndexOf(word[i])) {
+      codeWord += '('
+    } else {
+      codeWord += ')'
+    }
+  }
+  return codeWord
+}
