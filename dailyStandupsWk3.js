bb8eea0deeca25a71db1dbbe472b4433ba426caf
Dakota O'Neil <oneilkody@gmail.com>
2021-07-22 16:39:42 -0500

    add solution for todays standup challenge

--- dailyStandupsWk3.js
+++ dailyStandupsWk3.js
@@ -112,3 +112,21 @@ while (maxMin.length < arr.length) {
 }
 return maxMin
 };
+
+
+
+
+//July 22, 2021
+
+//Numbers to Letters
+
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+function switcher(x){
+  let aB = 'zyxwvutsrqponmlkjihgfedcba!? '
+  return x.map((el,i,arr)=> {
+    return aB[+el - 1]
+  }).join('')
+}
