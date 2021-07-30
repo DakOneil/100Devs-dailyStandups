e436d36faaf32e8b564168d38e02f95a48b54ae6
Dakota O'Neil <oneilkody@gmail.com>
2021-07-30 10:31:58 -0500

    add today's standup

--- dailyStandupsWk4.js
+++ dailyStandupsWk4.js
@@ -58,3 +58,39 @@ var capitals = function (word) {
   }
   return array
 };
+
+
+
+
+
+//July 29,2021
+// String array duplicates
+
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+// Good luck!
+
+// If you like this Kata, please try:
+
+// Alternate capitalization
+
+// Vowel consonant lexicon
+
+function dup(s) {
+  //..
+    for (let i = 0 ; i < s.length ; i++) {
+      for (let j = 0 ; j < s[i].length ; j++) {
+        if (s[i][j] === s[i][j+1]) {
+          s[i] = s[i].slice(0,j) + s[i].slice(j+1,s[i].length)
+          j--
+        }
+      }
+    }
+    return s
+  };
