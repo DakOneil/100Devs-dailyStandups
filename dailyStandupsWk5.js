7bde12398ab42f2e5d4cb34b1726d21d0fe7d683
Dakota O'Neil <oneilkody@gmail.com>
2021-08-03 12:35:50 -0500

    add todays standup solution

--- dailyStandupsWk5.js
+++ dailyStandupsWk5.js
@@ -24,3 +24,23 @@ function findDeletedNumber(arr, mixArr) {
     }
     return 0
   }
+
+
+
+
+// August 3, 2021
+
+//Double Sort
+
+// Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+
+// Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type.
+
+// Note that numbers written as strings are strings and must be sorted with the other strings.
+
+function dbSort(a){
+  // Code here
+    let nums = a.filter(x=>typeof x === 'number')
+    let strs = a.filter(x=>typeof x === 'string')
+    return nums.sort((b,c)=>b-c).concat(strs.sort())
+  }
