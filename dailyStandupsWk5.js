a73274db5f55ec0e7738de7d17177d4fd0a3db5c
Dakota O'Neil <oneilkody@gmail.com>
2021-08-03 22:13:45 -0500

    add sol for tomorrows standup and also old CW sol for same problem

--- dailyStandupsWk5.js
+++ dailyStandupsWk5.js
@@ -53,3 +53,37 @@ function dbSort(a){
       let strs = a.filter(x=>typeof x === 'string')
       return nums.concat(strs.sort())
     }
+
+
+
+// August 4, 2021
+
+// Find the middle element
+
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+var gimme = function (inputArray) {
+  // Implement this function
+  return inputArray.findIndex(x=> x > inputArray.sort((a,b)=>a-b)[0] && x < inputArray.sort((a,b)=>a-b)[2])
+};
+
+// old codwars sol
+
+var gimme = function (arr) {
+  // Implement this function
+  if ((arr[0] < arr[1] && arr[1] < arr[2]) || (arr[2] < arr[1] && arr[1] < arr[0])) return 1
+  else if ((arr[0] < arr[2] && arr[2] < arr[1]) || (arr[1]<arr[2] && arr[2] < arr[0])) return 2
+  else return 0
+};
