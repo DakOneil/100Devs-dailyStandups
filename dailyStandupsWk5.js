0414498855e04c0be41ed6d0b77701a57e39f663
Dakota O'Neil <oneilkody@gmail.com>
2021-08-06 13:40:54 -0500

    add todays standup solution

--- dailyStandupsWk5.js
+++ dailyStandupsWk5.js
@@ -112,3 +112,28 @@ function absentVowel(x){
     if (!x.split('').includes(vowels[i])) return i
   }
 }
+
+
+
+
+
+
+//August 6, 2021
+
+//The average length
+
+// Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+// If the average length is not an integer, use Math.round().
+// The input array's length > 1
+
+function averageLength(arr) { 
+  // good luck
+  let lengthNum = Math.round(arr.reduce((acc,c)=>acc + c.length, 0) / arr.length)
+  return arr.map(el=> el[0].repeat(lengthNum))
+}
