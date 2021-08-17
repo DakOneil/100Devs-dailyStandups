e9b81a94f4a31661638fcddbcdf9f04208dfc527
Dakota O'Neil <oneilkody@gmail.com>
2021-08-17 09:52:58 -0500

    add todays standup solution

--- dailyStandupsWk7.js
+++ dailyStandupsWk7.js
@@ -26,3 +26,46 @@ function revWords(str) {
 
 
 
+//August 17, 2021
+
+// Given a magazine of words and a ransom note, determine if it’s possible to “cut out” and create the ransom note from the magazine words.
+
+// const magazine =
+//  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+// describe("Ransom Note", () => {
+//  it("Should return true", () => {
+//   assert.equal(ransomNote("sit ad est sint", magazine), true);
+//  });
+
+// it("Should return false", () => {
+//   assert.equal(ransomNote("sit ad est love", magazine), false);
+//  });
+
+// it("Should return true", () => {
+//   assert.equal(ransomNote("sit ad est sint in in", magazine), true);
+//  });
+
+// it("Should return false", () => {
+//   assert.equal(ransomNote("sit ad est sint in in in in", magazine), false);
+//  });
+// });
+
+function canCutOut(note, mag) {
+    const noteArr = note.split(' ')
+    const magArr = mag.split(' ')
+    let magObj = {}
+    magArr.forEach(el=> {
+        if (!magObj[el]) magObj[el] = 1
+        else magObj[el]++
+    })
+    let noteObj = {}
+    noteArr.forEach(el=> {
+        if (!noteObj[el]) noteObj[el] = 1
+        else noteObj[el]++
+    })
+    return Object.keys(noteObj).map(el=> {
+      if (!magObj[el] || noteObj[el] > magObj[el]) return false
+      else return true
+    }).every(x=>x)
+}
