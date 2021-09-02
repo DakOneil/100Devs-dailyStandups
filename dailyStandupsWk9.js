92f1b6d620ff42369e80c7b975cec0e8e9967f59
Dakota O'Neil <oneilkody@gmail.com>
2021-09-02 08:20:05 -0500

    add todays standup sol

--- dailyStandupsWk9.js
+++ dailyStandupsWk9.js
@@ -28,3 +28,28 @@ var fib = function(n) {
     if(n==0 || n==1)  return n;
   else return fib(n-1)+fib(n-2);
   };
+
+
+
+
+
+//September 1, 2021
+
+// Please use recursion! We'll identify the base cases and walk through a recursive solution together during standup!
+
+// Factorial
+
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+
+// 5! = 5 * 4 * 3 * 2 * 1 = 120.
+
+// The value of 0! is 1.
+
+// #Your task
+
+// You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+const factorial = n => {
+  if(n==0||n==1) return 1;
+  else return n * factorial(n-1)
+};
