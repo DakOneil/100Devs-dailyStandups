efeb3cc4a68c69e49aa9dcea7077b2a94c0a847a
Dakota O'Neil <oneilkody@gmail.com>
2021-09-02 14:53:21 -0500

    add todays standup solution

--- dailyStandupsWk9.js
+++ dailyStandupsWk9.js
@@ -53,3 +53,23 @@ const factorial = n => {
   if(n==0||n==1) return 1;
   else return n * factorial(n-1)
 };
+
+
+
+
+
+//September 2, 2021
+
+
+// Greatest common divisor
+
+// Please use recursion! We'll identify the base case and walk through a recursive solution together during standup! Solution in the hint (write it out on paper)
+
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+function mygcd(x,y){
+  //your code here
+  return y == 0 ? x : mygcd(y, x % y)
+}
